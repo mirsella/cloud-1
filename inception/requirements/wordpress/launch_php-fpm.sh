bed902175ba679b26872e3dfc64c4776ad87dace
mirsella <mirsella@protonmail.com>
2024-07-19 17:55:36 +0200

    wordpress available on 8433 now

--- inception/requirements/wordpress/launch_php-fpm.sh
+++ inception/requirements/wordpress/launch_php-fpm.sh
@@ -2,15 +2,8 @@
 
 if [ ! -f ./wp-config.php ]; then
 	echo first launch, installing wordpress
-	wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --allow-root
-	wp core install --url=localhost --title=Inception --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${MYSQL_USER}@127.0.0.1 --allow-root
-	
-	wp config set WP_REDIS_HOST redis --allow-root
-	wp config set WP_REDIS_PORT 6379 --raw --allow-root
-	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-	wp config set WP_CACHE true --raw --allow-root
-	wp plugin install redis-cache --activate --allow-root
-	wp redis enable --allow-root
+	wp config create --dbname=wordpress --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --dbprefix=wp_ --allow-root
+	wp core install --url=localhost:8443 --title=Inception --admin_user="${MYSQL_USER}" --admin_password="${MYSQL_PASSWORD}" --admin_email="${MYSQL_USER}"@127.0.0.1 --allow-root
 
 	chown -R www-data:www-data /var/www/html
 	chmod -R 755 /var/www/html
