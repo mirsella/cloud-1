67242c41538898de5b3c95c044e96cec18571206
mirsella <mirsella@protonmail.com>
2024-07-22 16:23:43 +0200

    add phpmyadmin

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 
 ## automated deployment of the [42 inception project](https://github.com/mirsella/inception)
 
-to start:
+to start:  
 `ansible-playbook -v install.yml`  
 `ansible-playbook -v install.yml --limit vagrant`  
-to uninstall:
+to update the code:  
+`ansible-playbook -v update.yml`  
+to uninstall:  
 `ansible-playbook -i hosts uninstall.yml`
-
-TODOs:
-
-- add phpmyadmin to the `srcs/docker-compose.yml`
--- inception/requirements/wordpress/launch_php-fpm.sh
+++ inception/requirements/wordpress/launch_php-fpm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -f ./wp-config.php ]; then
-	echo first launch, installing wordpress at url "$WORDPRESS_URL"
+	echo first launch, installing wordpress at url "$HOSTNAME"
 	wp config create --dbname=wordpress --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --dbprefix=wp_ --allow-root
-	wp core install --url="$WORDPRESS_URL" --title=Inception --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER"@127.0.0.1 --allow-root
+	wp core install --url="$HOSTNAME" --title=Inception --admin_user="$MYSQL_USER" --admin_password="$MYSQL_PASSWORD" --admin_email="$MYSQL_USER"@127.0.0.1 --allow-root
 
 	chown -R www-data:www-data /var/www/html
 	chmod -R 755 /var/www/html
